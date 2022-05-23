d746fb217f20e7effddfe59888e7df79ab57220c
Graciany Baeza Jara <91575285+hachiman2002@users.noreply.github.com>
2022-05-23 16:53:07 -0400

    Update contraseña_basica.py

--- contraseña_basica.py
+++ contraseña_basica.py
@@ -1,3 +1,6 @@
+#2.- Escribir un programa que almacene la cadena de caracteres contraseña en una variable, pregunte al usuario por la 
+#contraseña e imprima por pantalla si la contraseña introducida por el usuario coincide con la guardada en la variable sin tener en cuenta mayúsculas y minúsculas.
+
 passs= "holaaaa"
 
 intento = str(input("Ingrese contrasena:"))
