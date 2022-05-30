f9b6bd9fc35a2c79c052b5696cedb5c6a91aaa44
Graciany Baeza Jara <91575285+hachiman2002@users.noreply.github.com>
2022-05-30 16:40:30 -0400

    Create de_numero_a_otro.py

---
+++ de_numero_a_otro.py
@@ -0,0 +1,7 @@
+"""
+Escribir un programa que pida al usuario un número entero positivo y muestre por pantalla todos los números impares desde 1 hasta ese número separados por comas.
+"""
+num = int(input("Ingresa numero:"))
+
+for i in range (1,num+1):
+    print(i)
