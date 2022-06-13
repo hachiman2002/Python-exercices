099d07042be824abbe1e7acc54b41c98f7e7beb0
Graciany Baeza Jara <91575285+hachiman2002@users.noreply.github.com>
2022-06-13 16:23:39 -0400

    Create def_par.py

---
+++ def_par.py
@@ -0,0 +1,15 @@
+"""
+Requerir al usuario que ingrese un número entero e informar 
+si es primo o no, utilizando una función booleana que lo decida.
+"""
+
+def Espar(num):
+
+    if numero%2==0:
+        print("Es par")
+    else:
+        print("Es impar")
+
+
+numero = int(input("Numero: "))
+Espar(numero)
