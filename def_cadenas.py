6faf37e7a4644eb95e7fa7fbde2a1096b5b71e95
Graciany Baeza Jara <91575285+hachiman2002@users.noreply.github.com>
2022-06-13 17:52:55 -0400

    Update def_cadenas.py

--- def_cadenas.py
+++ def_cadenas.py
@@ -1,3 +1,4 @@
+
 def eliminar_espacios(cad):
     cad_aux=""
     for letra in cad:
@@ -11,6 +12,20 @@ def invertir_cadena(cad):
         cad_nueva=letra+cad_nueva
     return cad_nueva
 
+"""
+Diseñar una funcion que permita evaluar si la cadena ingresada
+es un palindromo. ejemplo oso, nadan, anita lava la tina
+"""
+
+def cadena_palindromo(cad):
+    cad = eliminar_espacios(cad)
+    inv=invertir_cadena(cad)
+
+    if cad==inv:
+        return True
+    else:
+        return False
+
 cadena = input("Ingresa cadena:")
 #Necesitamos eliminar los espacios en blancos
 
@@ -19,5 +34,11 @@ print(sin_espacios)
 
 cadena_invertida = invertir_cadena(cadena)
 print(cadena_invertida)
+
+if cadena_palindromo(cadena):
+    print("Cadena es un palindromo")
+else:
+    print("Cadena no es un palindromo")
+
 
 #print(eliminar_espacios(input("Ingresa otra cadena:")))
