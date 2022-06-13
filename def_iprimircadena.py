2424f764aa675987fd73617ac6a66b00ecaa208a
Graciany Baeza Jara <91575285+hachiman2002@users.noreply.github.com>
2022-06-13 17:13:55 -0400

    Create def_iprimircadena.py

---
+++ def_iprimircadena.py
@@ -0,0 +1,10 @@
+#Imprimir cadena
+
+def imprimir_cadena(cad):
+    print(f"Enviaste la cadena {cad} ")
+
+
+cadena = input("Ingresa cadena:")
+#Necesitamos eliminar los espacios en blancos
+
+imprimir_cadena(cadena)
