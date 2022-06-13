0cf7605eb37026161fbcc69743ae848056997d92
Graciany Baeza Jara <91575285+hachiman2002@users.noreply.github.com>
2022-06-13 16:40:02 -0400

    Create def_suma.py

---
+++ def_suma.py
@@ -0,0 +1,10 @@
+Pedir al usuario que ingrese dos numeros y luego los sume
+"""
+
+def Suma(x,y):
+    suma = x + y
+    return suma
+
+num1 = int(input("Numero: "))
+num2 = int(input("Numero: "))
+print(Suma(num1,num2))
